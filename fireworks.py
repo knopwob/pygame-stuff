62d116c49426dfe0726a7d93627be66309ca2a04
Sascha Kruse <knopwob@knopwob.de>
2016-12-14 05:32:44 +0100

    knopgame

--- fireworks.py
+++ fireworks.py
@@ -4,24 +4,12 @@ import pygame
 import sys
 import random
 
+from knopgame.vector2d import Vector2D
+
 ##############################
 # CLASSES
 ##############################
 
-class Vector2D(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def as_tuple(self):
-        return (self.x, self.y)
-
-    def add(self, other):
-        self.x += other.x
-        self.y += other.y
-
-    def copy(self):
-        return Vector2D(self.x, self.y)
 
 
 class Particle(object):
---
+++ knopgame/vector2d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+class Vector2D(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def as_tuple(self):
+        return (self.x, self.y)
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+
+    def copy(self):
+        return Vector2D(self.x, self.y)
