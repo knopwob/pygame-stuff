eab1d26d42a8372c92ad75bf64cc0f94c884caee
Sascha Kruse <knopwob@knopwob.de>
2016-12-14 06:24:56 +0100

    template.py remove vector2d

--- template.py
+++ template.py
@@ -7,22 +7,6 @@ import sys
 # CLASSES
 ##############################
 
-class Vector2D(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def as_tuple(self):
-        return (self.x, self.y)
-
-    def add(self, other):
-        self.x += other.x
-        self.y += other.y
-
-    def copy(self):
-        return Vector2D(self.x, self.y)
-
-
 class Particle(object):
     def __init__(self, pos, vel=Vector2D(0,0)):
         self.pos = pos
